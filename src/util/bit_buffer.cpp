9b5c06b9264e11fa7d6178e4ff85e2c191e8f784
Vereena Inara <vereena.inara@gmail.com>
2025-05-10 13:50:28 -0400

    beahgs

--- src/util/bit_buffer.cpp
+++ src/util/bit_buffer.cpp
@@ -1,3 +1,28 @@
+#define BIT_BUFFER_SHARED                                           \
+    void free() {                                                   \
+        xfree(data, a);                                             \
+    }                                                               \
+                                                                    \
+    u32 get(u64 index) {                                            \
+        assert(index < entries);                                    \
+                                                                    \
+        u64 ia = index / entries_per_u64;                           \
+        u64 ie = (index - ia * entries_per_u64) * bits_per_entry;   \
+        return (data[ia] >> ie) & mask;                             \
+    }                                                               \
+                                                                    \
+    void set(u64 index, u32 value) {                                \
+        assert(index < entries);                                    \
+        assert((value & mask) == value);                            \
+                                                                    \
+        u64 ia = index / entries_per_u64;                           \
+        u64 ie = (index - ia * entries_per_u64) * bits_per_entry;   \
+        u64 m = ~(mask << ie);                                      \
+        data[ia] = (data[ia] & m) | (value << ie);                  \
+    }
+
+
+
 struct Bit_Buffer {
     u8 bits_per_entry;
     u8 entries_per_u64;
@@ -6,38 +31,55 @@ struct Bit_Buffer {
     u64 *data;
     Allocator *a;
 
+    
     Bit_Buffer(u8 _bits_per_entry, u64 _entries, Allocator *_a = ::allocator) {
-        assert(bits_per_entry >= 1 && bits_per_entry <= 64); // TODO?
-
         bits_per_entry  = _bits_per_entry;
         entries_per_u64 = 64 / bits_per_entry;
         entries         = _entries;
         mask            = (1 << bits_per_entry) - 1;
         a               = _a;
+
+        assert(bits_per_entry >= 1 && bits_per_entry <= 64); // TODO?
+        assert(entries <= (1 << bits_per_entry));
         
         u64 n = (entries + entries_per_u64 - 1) / entries_per_u64;
         data = cast(u64*, xalloc(sizeof(64) * n, a));
     }
 
-    void free() {
-        xfree(data, a);
-    }
-
-    u32 get(u64 index) {
-        assert(index < entries);
-
-        u64 ia = index / entries_per_u64;
-        u64 ie = (index - ia * entries_per_u64) * bits_per_entry;
-        return (data[ia] >> ie) & mask;
-    }
-
-    void set(u64 index, u32 value) {
-        assert(index < entries);
-        assert((value & mask) == value);
-
-        u64 ia = index / entries_per_u64;
-        u64 ie = (index - ia * entries_per_u64) * bits_per_entry;
-        u64 m = ~(mask << ie);
-        data[ia] = (data[ia] & m) | (value << ie);
-    }
+    BIT_BUFFER_SHARED
 };
+
+
+
+template<u64 _bits_per_entry>
+struct Static_Bit_Buffer {
+    static constexpr u64 bits_per_entry = _bits_per_entry;
+    static constexpr u64 max_entries    = 1 << bits_per_entry;
+    static constexpr u64 mask           = (1 << bits_per_entry) - 1;
+
+    static_assert(bits_per_entry >= 1 && bits_per_entry <= 64);
+
+
+    u8 entries_per_u64;
+    u64 entries;
+    u64 *data;
+    Allocator *a;
+
+
+    Static_Bit_Buffer(u64 _entries, Allocator *_a = ::allocator) {
+        entries_per_u64 = 64 / bits_per_entry;
+        entries         = _entries;
+        a               = _a;
+
+        assert(entries <= max_entries);
+
+        u64 n = (entries + entries_per_u64 - 1) / entries_per_u64;
+        data = cast(u64*, xalloc(sizeof(64) * n, a));
+    }
+
+    BIT_BUFFER_SHARED
+};
+
+
+
+#undef BIT_BUFFER_SHARED
--- src/util/paletted_bit_buffer.cpp
+++ src/util/paletted_bit_buffer.cpp
@@ -23,7 +23,7 @@ struct Paletted_Bit_Buffer {
     void set(u64 index, u32 value) {
         u64 ip = palette.index(value);
         if(ip == -1) {
-            if(palette.count + 1 > max_entries) resize();
+            if(palette.count + 1 > max_entries) increase_size();
             ip = palette.count;
             palette.push(value);
         }
@@ -31,7 +31,7 @@ struct Paletted_Bit_Buffer {
     }
 
 private:
-    void resize() {
+    void increase_size() {
         auto new_data = xnew(Bit_Buffer, data->bits_per_entry + 1, data->entries);
         max_entries <<= 1;
 
@@ -43,5 +43,45 @@ private:
         xfree(data);
 
         data = new_data;
+    }
+};
+
+
+
+template<u64 bits_per_entry>
+struct Static_Paletted_Bit_Buffer {
+    static constexpr u64 max_entries = 1 << bits_per_entry;
+
+
+    Static_Bit_Buffer<bits_per_entry> *data;
+    Array<u32> palette;
+    
+
+    Static_Paletted_Bit_Buffer(u64 entries) {
+        assert(entries <= max_entries);
+
+        data = xnew(Static_Bit_Buffer<bits_per_entry>, entries);
+        palette.resize(max_entries);
+    }
+
+    void free() {
+        data->free();
+        xfree(data);
+
+        palette.free();
+    }
+
+    u32 get(u64 index) {
+        return palette[data->get(index)];
+    }
+
+    void set(u64 index, u32 value) {
+        u64 ip = palette.index(value);
+        if(ip == -1) {
+            assert((palette.count + 1) <= max_entries);
+            ip = palette.count;
+            palette.push(value);
+        }
+        data->set(index, ip);
     }
 };
