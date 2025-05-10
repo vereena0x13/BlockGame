e2bc009158de18cd737fa165d8343eb709e96128
Vereena Inara <vereena.inara@gmail.com>
2025-05-10 14:48:21 -0400

    meh

--- src/util/paletted_bit_buffer.cpp
+++ src/util/paletted_bit_buffer.cpp
@@ -1,12 +1,20 @@
+// NOTE TODO: Should paletted bit buffers remove palette entries if,
+//            via the set method, a specific palette entry becomes unused?
+//            i.e. it no longer occurs in the Bit_Buffer? Or should we have
+//            a method users can call to remove unused palette entries perhaps?
+
+
+
 struct Paletted_Bit_Buffer {
     Bit_Buffer *data;
     Array<u32> palette;
-    u32 max_entries;
+    u32 palette_size;
 
+    
     Paletted_Bit_Buffer(u64 entries) {
         data = xnew(Bit_Buffer, 4, entries);
         palette.resize(16);
-        max_entries = 16;
+        palette_size = 16;
     }
 
     void free() {
@@ -23,7 +31,7 @@ struct Paletted_Bit_Buffer {
     void set(u64 index, u32 value) {
         u64 ip = palette.index(value);
         if(ip == -1) {
-            if(palette.count + 1 > max_entries) increase_size();
+            if(palette.count + 1 > palette_size) increase_size();
             ip = palette.count;
             palette.push(value);
         }
@@ -33,7 +41,7 @@ struct Paletted_Bit_Buffer {
 private:
     void increase_size() {
         auto new_data = xnew(Bit_Buffer, data->bits_per_entry + 1, data->entries);
-        max_entries <<= 1;
+        palette_size <<= 1;
 
         for(u64 i = 0; i < data->entries; i++) {
             new_data->set(i, palette[data->get(i)]);
@@ -50,7 +58,7 @@ private:
 
 template<u64 bits_per_entry>
 struct Static_Paletted_Bit_Buffer {
-    static constexpr u64 max_entries = 1 << bits_per_entry;
+    static constexpr u64 palette_size = 1 << bits_per_entry;
 
 
     Static_Bit_Buffer<bits_per_entry> *data;
@@ -58,10 +66,10 @@ struct Static_Paletted_Bit_Buffer {
     
 
     Static_Paletted_Bit_Buffer(u64 entries) {
-        assert(entries <= max_entries);
+        assert(entries <= palette_size);
 
         data = xnew(Static_Bit_Buffer<bits_per_entry>, entries);
-        palette.resize(max_entries);
+        palette.resize(palette_size);
     }
 
     void free() {
@@ -78,7 +86,7 @@ struct Static_Paletted_Bit_Buffer {
     void set(u64 index, u32 value) {
         u64 ip = palette.index(value);
         if(ip == -1) {
-            assert((palette.count + 1) <= max_entries);
+            assert((palette.count + 1) <= palette_size);
             ip = palette.count;
             palette.push(value);
         }
